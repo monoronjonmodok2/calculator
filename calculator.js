4ea0859f0a44e8851a87cae39f3a790dd7b2edef
monoronjonmodok <monoronjonmodok1@gmail.com>
2023-09-18 22:07:50 +0100

    all updated

--- calculator.js
+++ calculator.js
@@ -1,48 +1,38 @@
-// function calculator (a,b){
-//  return a+b;
-//  return a-b;
-//  return a
-
-// };
-// let value = calculator(10,20);
-// console.log(value);
-
-
 function sum(){
-var number1= document.getElementById("num1");
-var number2= document.getElementById("num2");
-var numInput1 = parseFloat(number1.value);
-var numInput2 = parseFloat(number2.value);
-document.getElementById("result").innerHTML =  numInput1 + numInput2 ;
-
-
+    const number1 = document.getElementById("num1");
+    const input1 = parseFloat(number1.value);
+    const number2 = document.getElementById("num2");
+    const input2 = parseFloat(number2.value);
+    document.getElementById("output").innerHTML = input1+input2;
 }
 
 function add(){
-    var number1= document.getElementById("num1");
-var number2= document.getElementById("num2");
-var numInput1 = parseFloat(number1.value);
-var numInput2 = parseFloat(number2.value);
-document.getElementById("result").innerHTML =  numInput1 - numInput2 ;
+    const number1 = document.getElementById("num1");
+    const input1 = parseFloat(number1.value);
+    const number2 = document.getElementById("num2");
+    const input2 = parseFloat(number2.value);
+    document.getElementById("output").innerHTML = input1-input2;
 }
 
 function multi(){
-    var number1= document.getElementById("num1");
-    var number2= document.getElementById("num2");
-    var numInput1 = parseFloat(number1.value);
-    var numInput2 = parseFloat(number2.value);
-    document.getElementById("result").innerHTML =  numInput1 * numInput2 ;
+    const number1 = document.getElementById("num1");
+    const input1 = parseFloat(number1.value);
+    const number2 = document.getElementById("num2");
+    const input2 = parseFloat(number2.value);
+    document.getElementById("output").innerHTML = input1*input2;
+}
+function div(){
+    const number1 = document.getElementById("num1");
+    const input1 = parseFloat(number1.value);
+    const number2 = document.getElementById("num2");
+    const input2 = parseFloat(number2.value);
+    document.getElementById("output").innerHTML = input1/input2;
 }
 
-function dive(){
-    var number1 = document.getElementById("num1");
-    var numInput1 = number1.value;
-    var number2 = document.getElementById("num2");
-    var numinput2 = number2.value;
-    document.getElementById("result").innerHTML= numInput1/numinput2;
-}
+function reset(){
+    const resetData = document.getElementById("myFrom");
+    resetData.reset();
+    document.getElementById("output").innerHTML = "show output";
+  
 
-function resetInput(){
-    var form = document.getElementById("myform")
-    form.reset();
 }
