4418c47e6c20e08ddab57ed90f49a6275bddde3c
monoronjonmodok <monoronjonmodok1@gmail.com>
2023-09-18 20:08:21 +0100

    project donet

---
+++ calculator.js
@@ -0,0 +1,43 @@
+// function calculator (a,b){
+//  return a+b;
+//  return a-b;
+//  return a
+
+// };
+// let value = calculator(10,20);
+// console.log(value);
+
+
+function sum(){
+var number1= document.getElementById("num1");
+var number2= document.getElementById("num2");
+var numInput1 = parseInt(number1.value);
+var numInput2 = parseInt(number2.value);
+document.getElementById("result").innerHTML =  numInput1 + numInput2 ;
+
+
+}
+
+function add(){
+    var number1= document.getElementById("num1");
+var number2= document.getElementById("num2");
+var numInput1 = parseInt(number1.value);
+var numInput2 = parseInt(number2.value);
+document.getElementById("result").innerHTML =  numInput1 - numInput2 ;
+}
+
+function multi(){
+    var number1= document.getElementById("num1");
+    var number2= document.getElementById("num2");
+    var numInput1 = parseInt(number1.value);
+    var numInput2 = parseInt(number2.value);
+    document.getElementById("result").innerHTML =  numInput1 * numInput2 ;
+}
+
+function dive(){
+    var number1 = document.getElementById("num1");
+    var numInput1 = number1.value;
+    var number2 = document.getElementById("num2");
+    var numinput2 = number2.value;
+    document.getElementById("result").innerHTML= numInput1/numinput2;
+}
